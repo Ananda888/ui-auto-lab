99ce92720c358536c445ace580121e80bf339929
Ananda888 <103986664+Ananda888@users.noreply.github.com>
2022-05-06 09:45:39 +1000

    commit

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+const { config } = require("chai");
+
 exports.config = {
     //
     // ====================
@@ -118,7 +120,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost',
+    baseUrl: 'http://localhost:3000/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -365,3 +367,4 @@ exports.config = {
     // onReload: function(oldSessionId, newSessionId) {
     // }
 }
+
